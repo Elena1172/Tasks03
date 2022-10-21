45d2f9d7060b392d0c62b268b33c8d7c4dac1a23
Elena <zagre8elena@yandex.ru>
2022-10-21 08:58:35 +0300

    added T19

--- T19/Program.cs
+++ T19/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+//является ли оно палиндромом.
+//Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+int NambeRevers(int x){
+    string s = "";
+    while(x > 0){
+        int m = x % 10;
+        x = x / 10;
+        s = s + m;
+    }
+    int y = Convert.ToInt32(s);
+    return y;
+}
+Console.Write("Введите натуральное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 0){
+    string st = a == NambeRevers(a)? "Да":"Нет";
+    Console.Write($"{a} -> {st}");
+}
+else Console.Write("Неправильный ввод числа");
